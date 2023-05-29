457871d4a982602f618698bee2c87662e1e5dc2f
Ashfak Uddin <ashmanpapet@gmail.com>
2023-05-29 18:45:32 -0400

    Can move around freely

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody body;
-     int[,] bombermanLayout = new int[,]
+    int[,] bombermanLayout = new int[,]
     {
         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
         { 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
@@ -19,7 +19,7 @@ public class Movement : MonoBehaviour
         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
     };
     private float movementSpeed = 1f;
-    [SerializeField] float rowPos = -4f, colPos = -4f;
+    [SerializeField] int rowPos = -4, colPos = -4;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,46 +33,84 @@ public class Movement : MonoBehaviour
         Debug.Log(rowPos + " " + colPos);
     }
 
-    private bool validMove(float newRow, float newCol)
+    private bool validMove(int newRow, int newCol)
     {
-        Debug.Log(newRow + " " + newCol);
-        // if (newRow < 0 || newRow >= 10 || newCol < 0 || newCol >= 10)
-        //     return false;
-        // return bombermanLayout[newRow, newCol] == 0;
-        return true;
+        int rowHold = newRow + 5;
+        int colHold = newCol + 5;
+        if (rowHold < 0 || rowHold >= 10 || colHold < 0 || colHold >= 10)
+             return false;
+         return bombermanLayout[rowHold, colHold] == 0;
+    }
+    private void SpawnSphere(int row, int col)
+    {
+        Debug.Log(row + " " + col);
     }
     // Update is called once per frame
+    private void bfs(int row, int col)
+    {
+        if (row + 5 < 0 || row + 5 >= 10 || col + 5 < 0 || col + 5 >= 10)
+            return;
+        if (validMove(row - 1, col))
+        {
+            SpawnSphere(row - 1, col);
+            bfs(row - 1, col);
+        }
+        if (validMove(row + 1, col))
+        {
+            SpawnSphere(row + 1, col);
+            bfs(row+ 1, col);
+        }
+        if (validMove(row, col - 1))
+        {
+            SpawnSphere(row, col - 1);
+            bfs(row, col - 1);
+        }
+        if (validMove(row, col + 1))
+        {
+            SpawnSphere(row, col + 1);
+            bfs(row, col + 1);
+        }
+    }
     void Update()
     { 
         if (Input.GetKeyDown("right"))
         {
             if (validMove(rowPos, colPos + 1))
             {
-                colPos += 1f;
+                colPos += 1;
                 body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
             }
         }
         if (Input.GetKeyDown("left"))
         {
-            colPos -= 1f;
-            body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            if (validMove(rowPos, colPos - 1))
+            {
+                colPos -= 1;
+                body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            }
         }
-         if (Input.GetKeyDown("up"))
+        if (Input.GetKeyDown("up"))
         {
-            rowPos += 1f;
-            body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            if (validMove(rowPos - 1, colPos))
+            {
+                rowPos -= 1;
+                body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            }
         }
-         if (Input.GetKeyDown("down"))
+        if (Input.GetKeyDown("down"))
         {
-            rowPos -= 1f;
-            body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            if (validMove(rowPos + 1, colPos))
+            {
+                rowPos += 1;
+                body.MovePosition(new Vector3((int) rowPos, 1, (int) colPos));
+            }
         }
-        // if (Input.GetKeyDown("space"))
-        // {
-        //     Vector3 newPosition = body.transform.position + new Vector3(0, 1, 0);
-
-        //     body.MovePosition(newPosition);
-        // }
+    
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space");
+           // bfs(rowPos, colPos);
+        }
   
        
         // if (Input.GetKeyDown("up"))
